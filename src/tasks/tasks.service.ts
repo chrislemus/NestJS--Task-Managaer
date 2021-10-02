afae8df32a2117b9ef4a637032b207ba4647ce5d
Cris Lemus <cris.lemus@genesys.com>
2021-10-02 14:58:20 -0400

    add error handling to getTaskById()

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,10 +13,16 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            //custom err msg is optional
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        return task;
     }
 
     deleteTask(id: string): void {
+        const task = this.getTaskById(id); //check if task exists, throw error if not
         this.tasks = this.tasks.filter((task) => task.id !== id);
     }
 
