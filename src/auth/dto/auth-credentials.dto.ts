0521c5a619b9fec936ae3abbdb02cc0d941336ec
Cris Lemus <cris.lemus@genesys.com>
2021-10-03 09:54:03 -0400

    hash/salt password when creating user

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -10,7 +10,8 @@ export class AuthCredentialsDto {
   @MinLength(8)
   @MaxLength(32)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'password should contain at least 1 upper case letter, at least 1 lower case letter, and at least 1 number or special character.',
+    message:
+      'password should contain at least 1 upper case letter, at least 1 lower case letter, and at least 1 number or special character.',
   })
   password: string;
 }
