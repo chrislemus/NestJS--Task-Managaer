0cdc2e3180277793a635ff0f1c0139d276a058ad
Cris Lemus <cris.lemus@genesys.com>
2021-10-03 17:46:56 -0400

    restrict task deletion and taskStatusUpdate to task created by user only

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -35,17 +35,22 @@ export class TasksController {
     return this.tasksService.getTaskById(id, user);
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body() updatedTaskStatusDto: UpdateTaskStatusDto,
-  // ): Promise<Task> {
-  //   return this.tasksService.updateTaskStatus(id, updatedTaskStatusDto.status);
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updatedTaskStatusDto: UpdateTaskStatusDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(
+      id,
+      updatedTaskStatusDto.status,
+      user,
+    );
+  }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(@Param('id') id: string, @GetUser() user: User): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
   }
 
   @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,26 +18,30 @@ export class TasksService {
   }
 
   async getTaskById(id: string, user: User): Promise<Task> {
-    const task = await this.taskRepository.findOne({ where: { id, user } });
+    const task = await this.taskRepository.findOne({ id, user });
     if (!task) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.taskRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.taskRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
   }
 
-  // async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-  //   const task = await this.getTaskById(id);
-  //   task.status = status;
-  //   await this.taskRepository.save(task);
-  //   return task;
-  // }
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
+    task.status = status;
+    await this.taskRepository.save(task);
+    return task;
+  }
 
   createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto, user);
